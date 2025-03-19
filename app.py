9330be387af68f25b7508a150d99b567cf6f46be
Kristoffer <krigjo25@gmail.com>
2025-03-19 17:24:23 +0100

    IC

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
+#   Application Entry
+
 #   Importing responsories
 import sys
+
+#   Custom dependencies
 from lib.command_line_tool import CommandlineInterface
 from lib.games.integerGames import LittleProfessor, GuessTheNumber, Crocks
 
 def main():
 
-        #   Ensure the arguments is less than two
+        help = "-h for available client commands"
+        
         try :
+            #   Ensure the arguments is less than two
             if len(sys.argv) < 2: 
-                raise Exception('Usage: python intgame.py -h to view the commands')
+                raise Exception(f'USEAGE: python app.py, {help}')
+            
         except Exception as e: 
             sys.exit(e)
 
-        cmd = CommandlineInterface()
-            
-        if cmd.CommandLineOptions().info: 
-            return cmd.Porgaminfo()
+        #   Initialize an instance of CommandlineInterface
+        instance = CommandlineInterface()
+
+        if instance.CommandLineOptions().info: 
+            return instance.Porgaminfo()
         
-        elif cmd.CommandLineOptions().tlp: 
+        elif instance.CommandLineOptions().tlp: 
             return LittleProfessor().run()
 
-        elif cmd.CommandLineOptions().gtn: 
+        elif instance.CommandLineOptions().gtn: 
             return GuessTheNumber().run()
         
-        elif cmd.CommandLineOptions().crocks: 
+        elif instance.CommandLineOptions().crocks: 
             return Crocks().run()
+        
+        else:
+            print(f'404 : Command Not Found, {help}')
 
 if __name__ == '__main__':
     main()
